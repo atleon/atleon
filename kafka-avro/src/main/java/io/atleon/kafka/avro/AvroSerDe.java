2e04d899e617e5526f6bbf32731ca6e4d61c4499
spierce <spierce@indeed.com>
2021-09-08 16:28:31 -0500

    Add more indirect references to Avro configs

--- kafka-avro/src/main/java/io/atleon/kafka/avro/AvroSerDe.java
+++ kafka-avro/src/main/java/io/atleon/kafka/avro/AvroSerDe.java
@@ -7,6 +7,14 @@ public abstract class AvroSerDe extends AbstractKafkaAvroSerDe {
 
     public static final String SCHEMA_REGISTRY_URL_CONFIG = AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG;
 
+    public static final String BASIC_AUTH_CREDENTIALS_SOURCE = AbstractKafkaAvroSerDeConfig.BASIC_AUTH_CREDENTIALS_SOURCE;
+
+    public static final String USER_INFO_CONFIG = AbstractKafkaAvroSerDeConfig.USER_INFO_CONFIG;
+
+    public static final String KEY_SUBJECT_NAME_STRATEGY = AbstractKafkaAvroSerDeConfig.KEY_SUBJECT_NAME_STRATEGY;
+
+    public static final String VALUE_SUBJECT_NAME_STRATEGY = AbstractKafkaAvroSerDeConfig.VALUE_SUBJECT_NAME_STRATEGY;
+
     protected static void validateByte(byte expectedByte, byte actualByte) {
         if (expectedByte != actualByte) {
             throw new IllegalArgumentException(String.format("Unexpected Byte. expected=%d actual=%d", expectedByte, actualByte));
