3f6780635f4eac6d6555eb9369a6b207e36da9cd
spierce <spierce@indeed.com>
2022-11-07 12:30:47 -0600

    #31: Implement `AloFlux::mapNotNull` and `AloFlux::mapPresent`

---
+++ core/src/test/java/io/atleon/core/AloFluxTest.java
@@ -0,0 +1,30 @@
+package io.atleon.core;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AloFluxTest {
+
+    @Test
+    public void aloFluxValuesCanBeMappedToNonNullValues() {
+        TestAlo empty = new TestAlo("");
+        TestAlo nonEmpty = new TestAlo("data");
+
+        List<String> result = new ArrayList<>();
+        AloFlux.wrap(Flux.just(empty, nonEmpty))
+            .mapNotNull((string) -> string.isEmpty() ? null : string)
+            .subscribe((alo) -> result.add(alo.get().toString()));
+
+        assertEquals(Collections.singletonList(nonEmpty.get()), result);
+        assertTrue(empty.isAcknowledged());
+        assertFalse(nonEmpty.isAcknowledged());
+    }
+}
