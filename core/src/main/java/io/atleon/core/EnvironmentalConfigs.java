c298a026478f6fac8f11f9a129a4d512b53e05e2
Sage Pierce <SagePierce@utexas.edu>
2023-03-22 15:15:53 -0500

    #112: Move environmental property loading to `EnvironmentalConfigs` - Make `EnvironmentalConfigs` own the loading of environment properties (the only place this is used) - Properly convert environment variables to properties

--- core/src/main/java/io/atleon/core/EnvironmentalConfigs.java
+++ core/src/main/java/io/atleon/core/EnvironmentalConfigs.java
@@ -1,9 +1,9 @@
 package io.atleon.core;
 
-import io.atleon.util.ConfigLoading;
-
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 public final class EnvironmentalConfigs implements ConfigInterceptor {
 
@@ -13,12 +13,33 @@ public final class EnvironmentalConfigs implements ConfigInterceptor {
     public Map<String, Object> intercept(String name, Map<String, Object> configs) {
         String prefix = String.format("%s%s.", PREFIX, name);
         Map<String, Object> result = new HashMap<>(configs);
-        result.putAll(ConfigLoading.loadPrefixedEnvironmentalProperties(prefix));
+        result.putAll(loadPrefixedEnvironmentalProperties(prefix));
         return result;
     }
 
     @Override
     public Map<String, Object> intercept(Map<String, Object> configs) {
         return configs;
+    }
+
+    private static Map<String, Object> loadPrefixedEnvironmentalProperties(String prefix) {
+        Map<String, Object> properties = new HashMap<>();
+        consumePrefixed(System.getenv(), key -> key.replaceAll("_", ".").toLowerCase(), prefix, properties::put);
+        consumePrefixed(System.getProperties(), Object::toString, prefix, properties::put);
+        return properties;
+    }
+
+    private static <K> void consumePrefixed(
+        Map<K, ?> source,
+        Function<K, String> keyToPropertyKey,
+        String prefix,
+        BiConsumer<String, Object> consumer
+    ) {
+        source.forEach((key, value) -> {
+            String propertyKey = keyToPropertyKey.apply(key);
+            if (propertyKey.startsWith(prefix)) {
+                consumer.accept(propertyKey.substring(prefix.length()), value);
+            }
+        });
     }
 }
