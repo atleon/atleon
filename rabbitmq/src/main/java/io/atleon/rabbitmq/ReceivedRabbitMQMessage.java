2010fbf398713491389c9f711dd47efcaa69a405
Sage Pierce <SagePierce@utexas.edu>
2023-04-20 13:33:20 -0500

    #176: Indicate when `ReceivedRabbitMQMessage` is a redelivery

--- rabbitmq/src/main/java/io/atleon/rabbitmq/ReceivedRabbitMQMessage.java
+++ rabbitmq/src/main/java/io/atleon/rabbitmq/ReceivedRabbitMQMessage.java
@@ -4,12 +4,20 @@ import com.rabbitmq.client.AMQP;
 
 public class ReceivedRabbitMQMessage<T> extends RabbitMQMessage<T> {
 
+    private final boolean redeliver;
+
     public ReceivedRabbitMQMessage(
         String exchange,
         String routingKey,
         AMQP.BasicProperties properties,
-        T body
+        T body,
+        boolean redeliver
     ) {
         super(exchange, routingKey, properties, body);
+        this.redeliver = redeliver;
+    }
+
+    public boolean isRedeliver() {
+        return redeliver;
     }
 }
