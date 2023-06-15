986700fa4a15334affc8d2ff80ba70a3f35b125a
Sage Pierce <SagePierce@utexas.edu>
2023-06-15 15:38:06 -0500

    Update SNS-SQS example to produce to SNS rather than SQS directly

--- examples/spring/src/test/java/io/atleon/examples/spring/awssnssqs/IntegrationTest.java
+++ examples/spring/src/test/java/io/atleon/examples/spring/awssnssqs/IntegrationTest.java
@@ -1,12 +1,13 @@
 package io.atleon.examples.spring.awssnssqs;
 
+import io.atleon.aws.sns.AloSnsSender;
+import io.atleon.aws.sns.ComposedSnsMessage;
+import io.atleon.aws.sns.SnsAddress;
 import io.atleon.aws.sns.SnsConfig;
-import io.atleon.aws.sqs.AloSqsSender;
-import io.atleon.aws.sqs.ComposedSqsMessage;
+import io.atleon.aws.sns.SnsConfigSource;
+import io.atleon.aws.sns.SnsMessage;
+import io.atleon.aws.sns.StringBodySerializer;
 import io.atleon.aws.sqs.SqsConfig;
-import io.atleon.aws.sqs.SqsConfigSource;
-import io.atleon.aws.sqs.SqsMessage;
-import io.atleon.aws.sqs.StringBodySerializer;
 import io.atleon.aws.testcontainers.AtleonLocalStackContainer;
 import io.atleon.aws.util.AwsConfig;
 import org.junit.jupiter.api.Test;
@@ -34,12 +35,14 @@ public class IntegrationTest {
 
     private static final AtleonLocalStackContainer CONTAINER = AtleonLocalStackContainer.createAndStart();
 
+    private static final String SNS_INPUT_TOPIC_NAME = "example-sns-input-topic";
+
     private static final String SQS_INPUT_QUEUE_NAME = "example-sqs-input-queue";
 
     private static final Consumer<Number> SPECIAL_NUMBER_CONSUMER = mock(Consumer.class);
 
     @Autowired
-    private String sqsInputQueueUrl; // Application creates this from set queue name, so just reuse it
+    private String snsInputTopicArn; // Application creates this from set topic name, so just reuse it
 
     @Test
     public void primeNumbersAreProcessed() {
@@ -51,16 +54,16 @@ public class IntegrationTest {
     }
 
     private void produceNumber(Number number) {
-        SqsConfigSource configSource = SqsConfigSource.unnamed()
+        SnsConfigSource configSource = SnsConfigSource.unnamed()
             .with(AwsConfig.REGION_CONFIG, CONTAINER.getRegion())
             .with(AwsConfig.CREDENTIALS_PROVIDER_TYPE_CONFIG, AwsConfig.CREDENTIALS_PROVIDER_TYPE_STATIC)
             .with(AwsConfig.CREDENTIALS_ACCESS_KEY_ID_CONFIG, CONTAINER.getAccessKey())
             .with(AwsConfig.CREDENTIALS_SECRET_ACCESS_KEY_CONFIG, CONTAINER.getSecretKey())
-            .with(SqsConfig.ENDPOINT_OVERRIDE_CONFIG, CONTAINER.getSqsEndpointOverride())
-            .with(AloSqsSender.BODY_SERIALIZER_CONFIG, StringBodySerializer.class);
-        try (AloSqsSender<Long> sender = AloSqsSender.from(configSource)) {
-            SqsMessage<Long> message = ComposedSqsMessage.fromBody(number.longValue());
-            sender.sendMessage(message, sqsInputQueueUrl).block();
+            .with(SnsConfig.ENDPOINT_OVERRIDE_CONFIG, CONTAINER.getSnsEndpointOverride())
+            .with(AloSnsSender.BODY_SERIALIZER_CONFIG, StringBodySerializer.class);
+        try (AloSnsSender<Long> sender = AloSnsSender.from(configSource)) {
+            SnsMessage<Long> message = ComposedSnsMessage.fromBody(number.longValue());
+            sender.sendMessage(message, SnsAddress.topicArn(snsInputTopicArn)).block();
         }
     }
 
@@ -76,7 +79,8 @@ public class IntegrationTest {
                 "example.aws.sns.sqs." + AwsConfig.CREDENTIALS_SECRET_ACCESS_KEY_CONFIG + "=" + CONTAINER.getSecretKey(),
                 "example.aws.sns.sqs." + SnsConfig.ENDPOINT_OVERRIDE_CONFIG + "=" + CONTAINER.getSnsEndpointOverride(),
                 "example.aws.sns.sqs." + SqsConfig.ENDPOINT_OVERRIDE_CONFIG + "=" + CONTAINER.getSqsEndpointOverride(),
-                "stream.sqs.input.topic.name=" + SQS_INPUT_QUEUE_NAME
+                "stream.sns.input.topic.name=" + SNS_INPUT_TOPIC_NAME,
+                "stream.sqs.input.queue.name=" + SQS_INPUT_QUEUE_NAME
             );
         }
     }
