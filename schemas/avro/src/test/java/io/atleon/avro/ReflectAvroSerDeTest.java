38c8f9af36951e4a5cacfd43c7b7ad487dbea371
Sage Pierce <SagePierce@utexas.edu>
2023-07-05 08:37:16 -0500

    Adding to Avro SerDe test

--- schemas/avro/src/test/java/io/atleon/avro/ReflectAvroSerDeTest.java
+++ schemas/avro/src/test/java/io/atleon/avro/ReflectAvroSerDeTest.java
@@ -4,7 +4,11 @@ import io.atleon.schema.SchemaBytes;
 import org.apache.avro.Schema;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ReflectAvroSerDeTest extends AvroSerDeTest {
@@ -48,5 +52,37 @@ public class ReflectAvroSerDeTest extends AvroSerDeTest {
 
         assertTrue(deserialized instanceof GenericDataHolder);
         assertEquals(data, GenericDataHolder.class.cast(deserialized).getData());
+    }
+
+    @Test
+    public void deserializingFieldsWithUnknownTypesResultsInNullValues() {
+        TestGenericData<TestData> data = new TestGenericData<>();
+        data.setData(TestData.create());
+
+        SchemaBytes<Schema> schemaBytes = serializer.serialize(data);
+
+        Schema schema = schemaBytes.schema();
+        List<Schema.Field> modifiedFields = schema.getFields().stream()
+            .map(field -> {
+                if (field.name().equals("data")) {
+                    Schema nonNullSchema = AvroSchemas.reduceNonNull(field.schema()).orElseThrow(IllegalStateException::new);
+                    List<Schema.Field> copiedFields = nonNullSchema.getFields().stream()
+                        .map(it -> new Schema.Field(it.name(), it.schema(), it.doc(), it.defaultVal()))
+                        .collect(Collectors.toList());
+                    Schema dataSchema = Schema.createRecord("TestDataUndefined", nonNullSchema.getDoc(), nonNullSchema.getNamespace(), nonNullSchema.isError(), copiedFields);
+                    return new Schema.Field(field.name(), AvroSchemas.makeNullable(dataSchema), field.doc(), field.defaultVal());
+                } else {
+                    return new Schema.Field(field.name(), field.schema(), field.doc(), field.defaultVal());
+                }
+            })
+            .collect(Collectors.toList());
+
+        Object deserialized = deserializer.deserialize(
+            schemaBytes.bytes(),
+            Schema.createRecord(schema.getName(), schema.getDoc(), schema.getNamespace(), schema.isError(), modifiedFields)
+        );
+
+        assertTrue(deserialized instanceof TestGenericData);
+        assertNull(TestGenericData.class.cast(deserialized).getData());
     }
 }
