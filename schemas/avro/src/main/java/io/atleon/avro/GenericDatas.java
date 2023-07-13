682ad1e28e04ed0b306390ef3931c26cfbc8e0d0
Sage Pierce <SagePierce@utexas.edu>
2023-07-13 11:50:01 -0500

    #212: Make inclusion of Avro logical types more robust

--- schemas/avro/src/main/java/io/atleon/avro/GenericDatas.java
+++ schemas/avro/src/main/java/io/atleon/avro/GenericDatas.java
@@ -1,36 +1,30 @@
 package io.atleon.avro;
 
+import io.atleon.util.Instantiation;
+import org.apache.avro.Conversion;
 import org.apache.avro.Conversions;
 import org.apache.avro.Schema;
 import org.apache.avro.data.TimeConversions;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.specific.SpecificData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Type;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 public final class GenericDatas {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GenericDatas.class);
 
     private GenericDatas() {
 
     }
 
-    public static <T extends GenericData> T addLogicalTypeConversion(T genericData) {
-        genericData.addLogicalTypeConversion(new Conversions.UUIDConversion());
-        genericData.addLogicalTypeConversion(new Conversions.DecimalConversion());
-
-        genericData.addLogicalTypeConversion(new TimeConversions.DateConversion());
-
-        genericData.addLogicalTypeConversion(new TimeConversions.TimeMillisConversion());
-        genericData.addLogicalTypeConversion(new TimeConversions.TimeMicrosConversion());
-
-        genericData.addLogicalTypeConversion(new TimeConversions.TimestampMillisConversion());
-        genericData.addLogicalTypeConversion(new TimeConversions.TimestampMicrosConversion());
-
-        genericData.addLogicalTypeConversion(new TimeConversions.LocalTimestampMillisConversion());
-        genericData.addLogicalTypeConversion(new TimeConversions.LocalTimestampMicrosConversion());
-
-        return genericData;
+    public static void addLogicalTypeConversion(GenericData genericData) {
+        instantiateConversionsFrom(Conversions.class).forEach(genericData::addLogicalTypeConversion);
+        instantiateConversionsFrom(TimeConversions.class).forEach(genericData::addLogicalTypeConversion);
     }
 
     static Function<Type, Schema> createTypeSchemaLoader(GenericData genericData) {
@@ -40,6 +34,21 @@ public final class GenericDatas {
             return type -> {
                 throw new UnsupportedOperationException("Cannot load schema using GenericData for type=" + type);
             };
+        }
+    }
+
+    private static Stream<Conversion<?>> instantiateConversionsFrom(Class<?> outerClass) {
+        return Stream.of(outerClass.getDeclaredClasses())
+            .filter(Conversion.class::isAssignableFrom)
+            .flatMap(GenericDatas::tryInstantiateConversion);
+    }
+
+    private static Stream<Conversion<?>> tryInstantiateConversion(Class<?> conversionClass) {
+        try {
+            return Stream.of((Conversion<?>) Instantiation.one(conversionClass));
+        } catch (Throwable error) {
+            LOGGER.debug("Failed to instantiate Conversion of type={}", conversionClass, error);
+            return Stream.empty();
         }
     }
 }
---
+++ schemas/avro/src/test/java/io/atleon/avro/GenericDatasTest.java
@@ -0,0 +1,21 @@
+package io.atleon.avro;
+
+import org.apache.avro.Conversions;
+import org.apache.avro.data.TimeConversions;
+import org.apache.avro.generic.GenericData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GenericDatasTest {
+
+    @Test
+    public void logicalTypeConversionsCanBeAdded() {
+        GenericData genericData = new GenericData();
+
+        GenericDatas.addLogicalTypeConversion(genericData);
+
+        assertTrue(genericData.getConversions().stream().anyMatch(Conversions.DecimalConversion.class::isInstance));
+        assertTrue(genericData.getConversions().stream().anyMatch(TimeConversions.TimeMillisConversion.class::isInstance));
+    }
+}
