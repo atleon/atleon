052352fc473c4e7c7e995602d371f09551bcb4de
Sage Pierce <SagePierce@utexas.edu>
2024-07-29 15:03:15 -0500

    [0.27.1] Update example

--- examples/spring/src/main/java/io/atleon/examples/spring/rabbitmq/config/RabbitMQConfiguration.java
+++ examples/spring/src/main/java/io/atleon/examples/spring/rabbitmq/config/RabbitMQConfiguration.java
@@ -1,14 +1,16 @@
 package io.atleon.examples.spring.rabbitmq.config;
 
+import io.atleon.rabbitmq.AloConnectionFactory;
 import io.atleon.rabbitmq.ExchangeDeclaration;
 import io.atleon.rabbitmq.QueueBinding;
 import io.atleon.rabbitmq.QueueDeclaration;
-import io.atleon.rabbitmq.RabbitMQConfig;
 import io.atleon.spring.ConfigContext;
 import io.atleon.spring.RabbitMQRoutingInitialization;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 @Configuration
 public class RabbitMQConfiguration {
@@ -20,8 +22,8 @@ public class RabbitMQConfiguration {
         @Value("${stream.rabbitmq.input.queue}") String inputQueue,
         @Value("${stream.rabbitmq.output.queue}") String outputQueue
     ) {
-        RabbitMQConfig config = RabbitMQConfig.create(context.getPropertiesPrefixedBy("example.rabbitmq"));
-        return RabbitMQRoutingInitialization.using(config.buildConnectionFactory())
+        Map<String, String> properties = context.getPropertiesPrefixedBy("example.rabbitmq");
+        return RabbitMQRoutingInitialization.using(AloConnectionFactory.create(properties))
             .addExchangeDeclaration(ExchangeDeclaration.direct(exchange))
             .addQueueDeclaration(QueueDeclaration.named(inputQueue))
             .addQueueDeclaration(QueueDeclaration.named(outputQueue))
