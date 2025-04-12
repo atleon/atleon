69b2af48507effcd9fd75da9391e808d4d4fc219
Sage Pierce <SagePierce@utexas.edu>
2025-04-12 16:58:19 -0500

    [#370] Remove unnecessary stream config

--- spring/src/main/java/io/atleon/spring/SpringAloStream.java
+++ spring/src/main/java/io/atleon/spring/SpringAloStream.java
@@ -1,7 +1,7 @@
 package io.atleon.spring;
 
-import io.atleon.core.PropertiedAloStreamConfig;
 import io.atleon.core.SelfConfigurableAloStream;
+import io.atleon.core.StreamPropertyResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
@@ -11,18 +11,18 @@ import java.util.Optional;
 
 /**
  * Spring-aware extension of {@link SelfConfigurableAloStream} that provides convenient access to
- * Spring properties. By implementing {@link PropertiedAloStreamConfig}, stream-specific properties
+ * Spring properties. By implementing {@link StreamPropertyResolver}, stream-specific properties
  * are located under a dot-notated prefix derived from the name of this stream. For example, given
  * default method usage, a stream named {@code MagnificentProcessingStream} will have its
  * properties available under the prefix {@code stream.magnificent.processing}, and to configure
  * a property like {@code concurrency} of that stream (for example), the property
  * {@code stream.magnificent.processing.concurrency} would be resolvable and accessible via
- * {@code getStreamProperty("concurrency", Integer::parse, 1)}. If an explicit stream property is
+ * {@code getStreamProperty("concurrency", Integer.class, 1)}. If an explicit stream property is
  * not found, an intermediate default is configurable under the namespace {@code stream.defaults},
  * so as an example, you can set the default value for {@code concurrency} used by all extending
  * streams by configuring {@code stream.defaults.concurrency}.
  */
-public abstract class SpringAloStream extends SelfConfigurableAloStream implements PropertiedAloStreamConfig {
+public abstract class SpringAloStream extends SelfConfigurableAloStream implements StreamPropertyResolver {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SpringAloStream.class);
 
@@ -62,7 +62,7 @@ public abstract class SpringAloStream extends SelfConfigurableAloStream implemen
      * Looks up the value associated with the provided key from {@link Environment}
      */
     @Override
-    public <T> Optional<T> getProperty(String key, Class<T> type) {
+    public final <T> Optional<T> getProperty(String key, Class<T> type) {
         return Optional.ofNullable(environment.getProperty(key, type));
     }
 }
