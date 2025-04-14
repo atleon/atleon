b11efe82a239191cb6e52da7748ac56f186300f7
Sage Pierce <SagePierce@utexas.edu>
2025-04-14 16:59:44 -0500

    [#370] Standardize `StreamPropertyResolver` convenience methods

--- core/src/main/java/io/atleon/core/StreamPropertyResolver.java
+++ core/src/main/java/io/atleon/core/StreamPropertyResolver.java
@@ -17,28 +17,34 @@ import java.util.Optional;
 public interface StreamPropertyResolver {
 
     /**
+     * Convenience method for {@link #getStreamProperty(String, Class) getStreamProperty(key, type).orElse(fallbackValue)}
+     */
+    default <T> T getStreamProperty(String key, Class<T> type, T fallbackValue) {
+        return getStreamProperty(key, type).orElse(fallbackValue);
+    }
+
+    /**
      * Returns the explicitly configured property value for the property key resulting from
      * concatenating this stream's {@link #streamPropertyNamespace() property namespace} and the
      * provided key in dot-notated form (by default implementation). If a value is not found, then
      * an attempt to read a "defaulted" value is made by concatenating this stream's
      * {@link #defaultablePropertyNamespace() default property namespace} and the provided key,
      * again in dot-notated form (by default implementation). If a value is ultimately found, the
-     * provided type is applied to return a strongly typed value. If a value is not found, then the
-     * provided fallback value will be returned. For example, given a property namespace of
+     * provided type is applied to return a strongly typed value. If a value is not found, then an
+     * empty result will be returned. For example, given a property namespace of
      * {@code stream.magnificent.processing} and a key of {@code concurrency}, the first property
      * lookup will be {@code stream.magnificent.processing.concurrency}, and if that property is
      * not available, the next lookup will be {@code stream.defaults.concurrency}, and if that
-     * property is also not available, the provided fallback value will be returned.
+     * property is also not available, an empty result will be returned..
      *
-     * @param key           The stream-specific property key to query a value for
-     * @param type          The type to parse available property value as
-     * @param fallbackValue Value to use if neither explicit nor default property is available
-     * @param <T>           The type of value that will be parsed into
+     * @param key  The stream-specific property key to query a value for
+     * @param type The type to parse available property value as
+     * @param <T>  The type of value that will be parsed into
      * @return The parsed property value for the given key
      */
-    default <T> T getStreamProperty(String key, Class<T> type, T fallbackValue) {
-        return getProperty(streamPropertyNamespace() + "." + key, type)
-            .orElseGet(() -> getProperty(defaultablePropertyNamespace() + "." + key, type, fallbackValue));
+    default <T> Optional<T> getStreamProperty(String key, Class<T> type) {
+        Optional<T> value = getProperty(streamPropertyNamespace() + "." + key, type);
+        return value.isPresent() ? value : getProperty(defaultablePropertyNamespace() + "." + key, type);
     }
 
     /**
@@ -58,9 +64,7 @@ public interface StreamPropertyResolver {
     }
 
     /**
-     * Returns the property value associated with the provided key, parsed as the provided type. If
-     * there is no value available for the provided key, the provided default value will be
-     * returned.
+     * Convenience method for {@link #getProperty(String, Class) getProperty(key, type).orElse(defaultValue)}
      */
     default <T> T getProperty(String key, Class<T> type, T defaultValue) {
         return getProperty(key, type).orElse(defaultValue);
