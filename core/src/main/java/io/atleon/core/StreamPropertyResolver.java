50c03b254f2dda2976c4076b0a2b595563daea70
Sage Pierce <SagePierce@utexas.edu>
2025-04-15 16:57:41 -0500

    [#370] Rework contract for stream property resolution - Consolidate available methods - Standardize on usage of `Optional` methods for fallback values

--- core/src/main/java/io/atleon/core/StreamPropertyResolver.java
+++ core/src/main/java/io/atleon/core/StreamPropertyResolver.java
@@ -4,38 +4,32 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 /**
- * Interface used to read properties within the context of configuring stream processes. This
+ * Interface used to read properties within the context of configuring a stream process. This
  * interface establishes a convention of using dot-notated property keys, and for stream
- * properties, lookup is first done by explicit keys, followed by lookup by default-able keys,
- * and then returning any explicitly passed fallback value. Stream properties are namespaced
- * according to the implementation of {@link #streamPropertyNamespace()}, which serves as a prefix
- * for initial stream property resolution. Default properties are namespaced according to the
- * implementation of {@link #defaultablePropertyNamespace()}, which serves as a prefix for
- * secondary stream property resolution. Convenience methods are provided for accessing optional
- * and required methods, with providable parsing types and fallback values.
+ * properties, lookup is first done by stream-specific keys, followed by lookup by default-able
+ * keys. Stream properties are namespaced according to the implementation of
+ * {@link #specificStreamPropertyNamespace()}, which serves as a prefix
+ * for primary stream property resolution. Default properties are namespaced according to the
+ * implementation of {@link #defaultStreamPropertyNamespace()}, which serves as a prefix for
+ * secondary stream property resolution. Convenience methods are provided for accessing required
+ * properties.
  */
 public interface StreamPropertyResolver {
 
     /**
-     * Convenience method for {@link #getStreamProperty(String, Class) getStreamProperty(key, type).orElse(fallbackValue)}
-     */
-    default <T> T getStreamProperty(String key, Class<T> type, T fallbackValue) {
-        return getStreamProperty(key, type).orElse(fallbackValue);
-    }
-
-    /**
-     * Returns the explicitly configured property value for the property key resulting from
-     * concatenating this stream's {@link #streamPropertyNamespace() property namespace} and the
-     * provided key in dot-notated form (by default implementation). If a value is not found, then
-     * an attempt to read a "defaulted" value is made by concatenating this stream's
-     * {@link #defaultablePropertyNamespace() default property namespace} and the provided key,
+     * Returns the stream-specific property value for the property key resulting from concatenating
+     * this stream's {@link #specificStreamPropertyNamespace() property namespace} and the provided
+     * key in dot-notated form (by default implementation). If a value is not found, then an
+     * attempt to read a "defaulted" value is made by concatenating this stream's
+     * {@link #defaultStreamPropertyNamespace() default property namespace} and the provided key,
      * again in dot-notated form (by default implementation). If a value is ultimately found, the
-     * provided type is applied to return a strongly typed value. If a value is not found, then an
-     * empty result will be returned. For example, given a property namespace of
-     * {@code stream.magnificent.processing} and a key of {@code concurrency}, the first property
-     * lookup will be {@code stream.magnificent.processing.concurrency}, and if that property is
-     * not available, the next lookup will be {@code stream.defaults.concurrency}, and if that
-     * property is also not available, an empty result will be returned..
+     * provided Class is applied to return a strongly typed value. If a value is not found, then an
+     * empty result will be returned. For example, given a stream-specific property namespace of
+     * {@code stream.magnificent.processing}, a default stream property namespace of
+     * {@code stream.defaults}, and a key of {@code concurrency}, the first property lookup will be
+     * {@code stream.magnificent.processing.concurrency}, and if that property is not available,
+     * the next lookup will be {@code stream.defaults.concurrency}, and if that property is also
+     * not available, an empty result will be returned.
      *
      * @param key  The stream-specific property key to query a value for
      * @param type The type to parse available property value as
@@ -43,8 +37,8 @@ public interface StreamPropertyResolver {
      * @return The parsed property value for the given key
      */
     default <T> Optional<T> getStreamProperty(String key, Class<T> type) {
-        Optional<T> value = getProperty(streamPropertyNamespace() + "." + key, type);
-        return value.isPresent() ? value : getProperty(defaultablePropertyNamespace() + "." + key, type);
+        Optional<T> value = getProperty(specificStreamPropertyNamespace() + "." + key, type);
+        return value.isPresent() ? value : getProperty(defaultStreamPropertyNamespace() + "." + key, type);
     }
 
     /**
@@ -64,22 +58,16 @@ public interface StreamPropertyResolver {
     }
 
     /**
-     * Convenience method for {@link #getProperty(String, Class) getProperty(key, type).orElse(defaultValue)}
+     * Namespace for explicitly defined properties of a stream, which is used as a property key
+     * prefix when looking up primary stream property values.
      */
-    default <T> T getProperty(String key, Class<T> type, T defaultValue) {
-        return getProperty(key, type).orElse(defaultValue);
-    }
+    String specificStreamPropertyNamespace();
 
     /**
-     * Provides an overridable property namespace, which is used as a property key prefix when
-     * looking up stream-specific property values.
+     * Namespace under which default stream properties may be located, which is used as a property
+     * key prefix when looking up secondary stream property values.
      */
-    String streamPropertyNamespace();
-
-    /**
-     * Namespace under which default stream properties may be located.
-     */
-    String defaultablePropertyNamespace();
+    String defaultStreamPropertyNamespace();
 
     /**
      * Returns the property value associated with the provided key, parsed as the provided type,
