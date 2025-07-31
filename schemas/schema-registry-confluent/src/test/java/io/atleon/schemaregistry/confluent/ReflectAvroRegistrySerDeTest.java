c2af7d9b8873946c488e2998a300572c192c8f54
Sage Pierce <SagePierce@utexas.edu>
2025-07-31 10:24:54 -0500

    [#402] Remove `atleon-schema-registry-confluent-embedded` module

--- schemas/schema-registry-confluent/src/test/java/io/atleon/schemaregistry/confluent/ReflectAvroRegistrySerDeTest.java
+++ schemas/schema-registry-confluent/src/test/java/io/atleon/schemaregistry/confluent/ReflectAvroRegistrySerDeTest.java
@@ -1,9 +1,7 @@
 package io.atleon.schemaregistry.confluent;
 
-import io.atleon.schemaregistry.confluent.embedded.EmbeddedSchemaRegistry;
 import org.junit.jupiter.api.Test;
 
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,9 +9,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class EmbeddedReflectAvroRegistrySerDeTest {
+public class ReflectAvroRegistrySerDeTest {
 
-    private static final URL SCHEMA_REGISTRY_URL = EmbeddedSchemaRegistry.startAndGetConnectUrl();
+    private static final String SCHEMA_REGISTRY_URL = "mock://test";
 
     @Test
     public void dataCanBeSerializedAndDeserialized() {
@@ -21,7 +19,7 @@ public class EmbeddedReflectAvroRegistrySerDeTest {
         RegistryDeserializer<Object, ?> deserializer = new AvroRegistryDeserializer<>();
 
         Map<String, Object> configs = new HashMap<String, Object>() {{
-            put(RegistrySerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, SCHEMA_REGISTRY_URL.toString());
+            put(RegistrySerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, SCHEMA_REGISTRY_URL);
             put(RegistrySerDeConfig.SCHEMA_REFLECTION_CONFIG, true);
         }};
 
