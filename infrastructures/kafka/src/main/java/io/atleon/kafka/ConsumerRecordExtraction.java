e18c16fd2c343485331d36106f40b6a605f8712d
Sage Pierce <SagePierce@utexas.edu>
2026-01-17 15:41:12 -0600

    [#468] Remove dependence on Reactor Kafka

--- infrastructures/kafka/src/main/java/io/atleon/kafka/ConsumerRecordExtraction.java
+++ infrastructures/kafka/src/main/java/io/atleon/kafka/ConsumerRecordExtraction.java
@@ -2,15 +2,12 @@ package io.atleon.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.TopicPartition;
-import reactor.kafka.receiver.ReceiverRecord;
 
 public final class ConsumerRecordExtraction {
 
     private ConsumerRecordExtraction() {}
 
     public static TopicPartition topicPartition(ConsumerRecord<?, ?> consumerRecord) {
-        return consumerRecord instanceof ReceiverRecord
-                ? ReceiverRecord.class.cast(consumerRecord).receiverOffset().topicPartition()
-                : new TopicPartition(consumerRecord.topic(), consumerRecord.partition());
+        return new TopicPartition(consumerRecord.topic(), consumerRecord.partition());
     }
 }
--- infrastructures/kafka/src/test/java/io/atleon/kafka/ConsumerMutexEnforcerTest.java
+++ infrastructures/kafka/src/test/java/io/atleon/kafka/ConsumerMutexEnforcerTest.java
@@ -3,7 +3,6 @@ package io.atleon.kafka;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.junit.jupiter.api.Test;
-import reactor.kafka.receiver.ReceiverOptions;
 
 import java.time.Duration;
 import java.util.Collections;
@@ -28,41 +27,10 @@ class ConsumerMutexEnforcerTest {
         assertDoesNotThrow(() -> newConsumer.poll(Duration.ZERO));
     }
 
-    @Test
-    public void newProhibitableConsumerFactory_givenDetectionOfOrphanedConsumer_expectsIllegalStateOnOldConsumer() {
-        ConsumerMutexEnforcer enforcer = new TestConsumerMutexEnforcer();
-
-        Consumer<Object, Object> oldConsumer =
-                enforcer.newProhibitableConsumerFactory().createConsumer(ReceiverOptions.create());
-        Consumer<Object, Object> newConsumer =
-                enforcer.newProhibitableConsumerFactory().createConsumer(ReceiverOptions.create());
-
-        assertThrows(IllegalStateException.class, () -> oldConsumer.poll(Duration.ZERO));
-        assertDoesNotThrow(() -> newConsumer.poll(Duration.ZERO));
-    }
-
-    @Test
-    public void newProhibitableConsumerFactory_givenLapseOfGracePeriod_expectsIllegalStateFurtherInvocation() {
-        ConsumerMutexEnforcer enforcer = new TestConsumerMutexEnforcer();
-        ConsumerMutexEnforcer.ProhibitableConsumerFactory factory = enforcer.newProhibitableConsumerFactory();
-
-        Consumer<Object, Object> consumer = factory.createConsumer(ReceiverOptions.create());
-        factory.prohibitFurtherConsumption(Duration.ofNanos(-1));
-
-        assertThrows(IllegalStateException.class, () -> consumer.poll(Duration.ZERO));
-    }
-
     private static final class TestConsumerMutexEnforcer extends ConsumerMutexEnforcer {
 
         @Override
         protected <K, V> Consumer<K, V> newConsumer(Map<String, Object> config) {
-            Consumer<K, V> consumer = mock(Consumer.class);
-            when(consumer.poll(any(Duration.class))).thenReturn(new ConsumerRecords<>(Collections.emptyMap()));
-            return consumer;
-        }
-
-        @Override
-        protected <K, V> Consumer<K, V> newConsumer(ReceiverOptions<K, V> options) {
             Consumer<K, V> consumer = mock(Consumer.class);
             when(consumer.poll(any(Duration.class))).thenReturn(new ConsumerRecords<>(Collections.emptyMap()));
             return consumer;
