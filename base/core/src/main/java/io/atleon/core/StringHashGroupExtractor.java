13efcd9088d534ebc0687162efcc45793925b78a
Sage Pierce <SagePierce@utexas.edu>
2026-03-29 13:12:58 -0500

    [#529] Core: Centralize Murmur3 usage - Simplify subsequent Guava usage deprecation

--- base/core/src/main/java/io/atleon/core/StringHashGroupExtractor.java
+++ base/core/src/main/java/io/atleon/core/StringHashGroupExtractor.java
@@ -1,15 +1,8 @@
 package io.atleon.core;
 
-import com.google.common.hash.HashCode;
-import com.google.common.hash.HashFunction;
-import com.google.common.hash.Hashing;
-
-import java.util.Objects;
 import java.util.function.Function;
 
 public abstract class StringHashGroupExtractor<T> implements Function<T, Integer> {
-
-    private final HashFunction hashFunction = Hashing.murmur3_32();
 
     private final int modulus;
 
@@ -23,9 +16,7 @@ public abstract class StringHashGroupExtractor<T> implements Function<T, Integer
 
     @Override
     public Integer apply(T t) {
-        String string = Objects.toString(extractString(t), "");
-        HashCode hash = hashFunction.hashUnencodedChars(string);
-        return Math.abs(hash.asInt() % modulus);
+        return Murmur3.hashIntoBucket(extractString(t), modulus);
     }
 
     protected abstract String extractString(T t);
